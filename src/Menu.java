1fcbc8c601ef48016db8edf47bde2f94076d408b
Szabolcs Varhegyi <sz.varhegyi@gmail.com>
2022-05-07 22:26:46 +0200

    menu implementation is started

---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Menu {
+
+    public void addItem() {
+
+    }
+
+    public int render() {
+        return 0;
+    }
+
+}
