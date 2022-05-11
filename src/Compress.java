281e4251c4f2eec1260c1d684a1f5143dcd890ec
Szabolcs Varhegyi <sz.varhegyi@gmail.com>
2022-05-11 19:46:16 +0200

    misi miatt

--- src/Compress.java
+++ src/Compress.java
@@ -19,6 +19,11 @@ public class Compress {
         return ret;
     }
 
+    public static String decompress(String compressedWord) {
+        //TODO: Misi please implement decompress method
+        return "";
+    }
+
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
@@ -34,6 +39,31 @@ public class Compress {
             System.out.print(compress(word) + " ");
         }
 
+
+        System.out.println("\nAnother solutions:");
+
+        //or without method call
+        int counter = 0;
+        String ret = "";
+        for(int j = 0; j < textsToCompress.length; j++) {
+
+           String textToCompress = textsToCompress[j];
+
+
+            for(int i = 0; i < textToCompress.length(); i++) {
+                do {
+                    counter++;
+                } while (i + counter < textToCompress.length() && textToCompress.charAt(i) == textToCompress.charAt(i + counter));
+
+                ret += counter + "" + textToCompress.charAt(i);
+                i += counter - 1;
+                counter = 0;
+            }
+
+            System.out.println(ret);
+            ret = "";
+
+        }
     }
 
 }
