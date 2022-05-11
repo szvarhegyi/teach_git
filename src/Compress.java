3ded402bfa2d07bb8850bc9e4b86285444b93a06
Szabolcs Varhegyi <sz.varhegyi@gmail.com>
2022-05-11 19:30:43 +0200

    space in input

--- src/Compress.java
+++ src/Compress.java
@@ -2,22 +2,36 @@ import java.util.*;
 
 public class Compress {
 
+    public static String compress(String textToCompress) {
+        int counter = 0;
+        String ret = "";
+
+        for(int i = 0; i < textToCompress.length(); i++) {
+            do {
+                counter++;
+            } while (i + counter < textToCompress.length() && textToCompress.charAt(i) == textToCompress.charAt(i + counter));
+
+            ret += counter + "" + textToCompress.charAt(i);
+            i += counter - 1;
+            counter = 0;
+        }
+
+        return ret;
+    }
+
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        String textToCompress = in.next();
-        int counter = 0;
+        String[] textsToCompress = in.nextLine().split("\\s");
 
-        for(int i = 0; i < textToCompress.length(); i++) {
-
-            do {
-                counter++;
-            } while (i+counter < textToCompress.length() && textToCompress.charAt(i) == textToCompress.charAt(i+counter));
-
-            System.out.print(counter + "" + textToCompress.charAt(i));
-            i += counter - 1;
-            counter = 0;
-
+        //First solution with for
+        for(int i = 0; i < textsToCompress.length; i++) {
+            System.out.print(compress(textsToCompress[i]) + " ");
+        }
+        System.out.println("\nAnother solutions:");
+        //or other solution with foreach
+        for (String word : textsToCompress) {
+            System.out.print(compress(word) + " ");
         }
 
     }
